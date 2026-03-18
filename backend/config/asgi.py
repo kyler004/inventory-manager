a6a03b58e416236fd609d1189d367b62a91e9805
kyler004 <lowelrayannkenneyemtsa@gmail.com>
2026-03-18 20:32:55 +0100

    feat: Setting up asgi server

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -8,9 +8,19 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.development')
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+
+from apps.alerts.routing import websocket_urlpatterns  # We'll create this later
+
+application = ProtocolTypeRouter({
+    'http': django_asgi_app,
+    'websocket': AuthMiddlewareStack(
+        URLRouter(websocket_urlpatterns)
+    ),
+})
