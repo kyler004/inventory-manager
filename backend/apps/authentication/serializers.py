2a90af126cd14826528c6fb434e5961ee34758ac
kyler004 <lowelrayannkenneyemtsa@gmail.com>
2026-04-03 13:57:03 +0100

    Implement a custom token pair serializer to return user details

--- backend/apps/authentication/serializers.py
+++ backend/apps/authentication/serializers.py
@@ -6,5 +6,23 @@ from apps.users.models import User
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer): 
     """
     Extends the default JWT login serializer. 
-    
+    By default it only returns acces + refresh tokens.
+    We also return user info so React doesn't need a second API call to knwo who just logged in.
+    Connects to: POST /auth/login/ from our API design.
     """
+
+    def validate(self, attrs): 
+        # Call parent- does the actual credential check
+        data = super().validate(attrs)
+
+        #Append user info to the response
+        data['user'] = {
+            'id': self.user.id, 
+            'name': self.user.name, 
+            'email': self.user.email, 
+            'role': self.user.role.name if self.user.role else None, 
+            'location_id': self.user.location_id, 
+        }
+
+        return data
+
