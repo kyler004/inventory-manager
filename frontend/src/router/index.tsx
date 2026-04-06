b9027748d37b2b2b41e2dd4af868a6949d14aa51
kyler004 <lowelrayannkenneyemtsa@gmail.com>
2026-04-06 15:10:15 +0100

    implement router index file

---
+++ frontend/src/router/index.tsx
@@ -0,0 +1,38 @@
+import { CreaterowserRouter } from 'react-router-dom'
+import { ProtectedRoute } from './ProtectedRoute'
+import { RoleGuard } from './RoleGuard'
+import { AppShell } from '@/components/layout/AppShell'
+
+import { LoginPage } from '@/pages/auth/LoginPage'
+import { DashboardPage } from '@/pages/dashboard/DashboardPage'
+import { ProductPage } from '@/pages/products/ProductsPage'
+import { StockPage } from '@/pages/stock/StockPage'
+import { UnauthorizedPage } from '@/pages/UnauthorizedPage'
+
+export const router = createBrowserRouter([
+    {
+        path: '/login', 
+        element: <LoginPage />, 
+    }, 
+    {
+        path: '/', 
+        element: (
+            <ProtectedRoute>
+                <AppShell />
+            </ProtectedRoute>
+        ), 
+        children: [
+            {index: true, element: <DashboardPage /> }, 
+            {path: 'products', element: <ProductPage /> }, 
+            {path: 'stock', element: <StockPage /> }, 
+            {path: 'settings/users', 
+                element: (
+                    <RoleGuard>
+                        <div className="">USers Page - coming soon</div>
+                    </RoleGuard>
+                ) }, 
+
+        ]
+    }, 
+    { path: '/unauthorized', element: <UnauthorizedPage />},
+])
