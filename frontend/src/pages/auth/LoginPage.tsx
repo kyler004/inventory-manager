140cd81165f5498f95aad89fb18be080e81989a9
kyler004 <lowelrayannkenneyemtsa@gmail.com>
2026-04-07 22:25:04 +0100

    feat: Login page implement

---
+++ frontend/src/pages/auth/LoginPage.tsx
@@ -0,0 +1,111 @@
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { authApi } from "@/api/auth.api";
+import { useAuthStore } from "@/store/authStore";
+import toast from "react-hot-toast";
+
+// Zod schema - defines and validates the form shape
+const loginSchema = z.object({
+  email: z.string().email("Please enter a valid email"),
+  password: z.string().min(8, "Password must be at least 8 characters"),
+});
+
+type LoginForm = z.infer<typeof loginSchema>;
+
+export const LoginPage = () => {
+  const navigate = useNavigate();
+  const setAuth = useAuthStore((state) => state.setAuth);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginForm>({
+    resolver: zodResolver(loginSchema),
+  });
+
+  const onSubmit = async (data: LoginForm) => {
+    setIsLoading(true);
+
+    try {
+      const response = await authApi.login(data.email, data.password);
+      // Store tokens and user in Zustand (persisted to Localstorage)
+      setAuth(response.user, response.access, response.refresh);
+      toast.success(`Welcome back, ${response.user.name}!`);
+      navigate("/");
+    } catch {
+      toast.error("Invalid email or password");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div className="min-h-screen bg-gray-50 flex items-center justify-center">
+      <div className="bg-white rounded-2xl shadow-sm border bprder-gray-200 p-8 w-full max-w-md">
+        {/* Header */}
+        <div className="text-center mb-8">
+          <h1 className="text-2xl font-bold text-gray-900">
+            <span className="text-blue-600">Inventory</span>Pro
+          </h1>
+          <p className="text-gray-500 mt-2">Sign in to your account</p>
+        </div>
+
+        {/* Form */}
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
+          <div className="">
+            <label
+              htmlFor=""
+              className="block text-sm font-medium text-gray-700 mb-1"
+            >
+              Email
+            </label>
+            <input
+              {...register("email")}
+              type="email"
+              placeholder="you@store.com"
+              className="w-full px-4 py-2.5 border border-gray-300 rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            />
+            {errors.email && (
+              <p className="text-red-500 text-xs mt-1">
+                {errors.email.message}
+              </p>
+            )}
+          </div>
+
+          <div className="">
+            <label
+              htmlFor=""
+              className="block text-sm font-medium text-gray-700 mb-1"
+            >
+              Password
+            </label>
+            <input
+              {...register("password")}
+              type="password"
+              placeholder="••••••••"
+              className="w-full px-4 py-2.5 border border-gray-300 rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            />
+            {errors.password && (
+              <p className="text-red-500 text-xs mt-1">
+                {errors.password.message}
+              </p>
+            )}
+          </div>
+
+          <button
+            type="submit"
+            disabled={isLoading}
+            className="w-full bg-blue-600 hover:bg-blue-700 disabled:bg-blue-400 text-white font-medium py-2.5 rounded-lg transition-colors text-sm"
+          >
+            {isLoading ? "Signing in..." : "Sign in"}
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
