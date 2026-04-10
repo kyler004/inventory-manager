89cdec8b2b5554e1f19abbe3c52289083d3898ff
kyler004 <lowelrayannkenneyemtsa@gmail.com>
2026-04-10 09:16:18 +0100

    Added routes for the websocket

--- backend/apps/alerts/routing.py
+++ backend/apps/alerts/routing.py
@@ -1,1 +1,6 @@
-websocket_urlpatterns = []
+from django.urls import re_path
+from . import consumers
+
+websocket_urlpatterns = [
+    re_path(r'ws/alerts/$', consumers.AlertConsumer.as_asgi()), 
+]
